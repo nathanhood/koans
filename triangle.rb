30c8e7c86f9ec431242e9614d0db50966ef42139
Nathan Hood <nathanhood@me.com>
2014-07-08 23:41:36 -0500

    about blocks

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError, "All sides must be greater than 0"
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError, "Cannot have negative values"
+  elsif a == b && (a+b) <= c || a == c && (a+c) <= b || c == b && (c+b) <= a
+    raise TriangleError, "Two equal sides of isosceles must be greater than third side"
+  elsif a == b && b == c && c == a
+    :equilateral
+  elsif a == b || a == c || c == b
+    :isosceles
+  elsif a != b && b != c && c != a
+    :scalene
+  else
+    nil
+  end
+
 end
 
 # Error class used in part 2.  No need to change this code.
